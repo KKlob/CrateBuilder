a3243c174849dd424e8474a98f3ae8e5334ee262
klobk <klobk23@gmail.com>
2024-03-28 14:09:25 -0400

    rewrite started. Simplifying design choices.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,20 @@
 import './App.css';
-import DimsForm from './components/dims_form';
-import CrateOutput from './components/crate_output';
 import { Grid, Container } from '@mui/material';
-import { useState } from 'react';
-import AddTwoByFoursForm from './components/add_two_by_fours';
+import DimsForm from './components/dims_form';
 
 function App() {
-  const [dims, setDims] = useState({
-    lm: 35,
-    lt: 1,
-    lb: 4,
-    wm: 20,
-    wt: 3,
-    wb: 8,
-    hm: 15,
-    ht: 3,
-    hb: 16,
-    weight: false
-  });
 
-  const [additionalTwoBysLength, updateAddTwoBysLength] = useState(0);
+
 
   return (
     <div className="App">
       <Container>
         This is the Crate Builder!
-        <Grid container spacing={2} align="">
-          <Grid item xs={12}>
-            <DimsForm dims={dims} setDims={setDims} />
+        <Grid container spacing={2} alignItems="center" justifyContent="center">
+          <Grid item xs={12} align="center">
+            <DimsForm />
           </Grid>
           <Grid item xs={12}>
-            <CrateOutput dims={dims} addTwoBys={additionalTwoBysLength} />
-          </Grid>
-          <Grid item xs={12}>
-            <AddTwoByFoursForm updateTwoBysTotal={updateAddTwoBysLength} />
           </Grid>
         </Grid>
       </Container>
--- src/components/dims_form.js
+++ src/components/dims_form.js
@@ -3,26 +3,19 @@ import { Button } from "@mui/material";
 import { useFormik } from "formik";
 import { Container } from "react-bootstrap";
 import * as Yup from 'yup';
+import Fraction from 'fraction.js';
 
-function DimsForm({ dims, setDims }) {
-
-    const DimsSchema = Yup.object().shape({
-        lm: Yup.number().required('Required').min(1, "Needs to be greater than 0"),
-        lt: Yup.number(),
-        lb: Yup.number(),
-        wm: Yup.number().required('Required').min(1, "Needs to be greater than 0"),
-        wt: Yup.number(),
-        wb: Yup.number(),
-        hm: Yup.number().required('Required').min(1, "Needs to be greater than 0"),
-        ht: Yup.number(),
-        hb: Yup.number()
-    });
+function DimsForm() {
 
     const formik = useFormik({
-        initialValues: dims,
-        validationSchema: DimsSchema,
+        initialValues: {
+            length: "",
+            width: "",
+            height: "",
+            twoBys: []
+        },
         onSubmit: values => {
-            setDims(values);
+            console.log(values);
         },
     });
 
@@ -30,92 +23,25 @@ function DimsForm({ dims, setDims }) {
         <div id="DimsForm">
             <form onSubmit={formik.handleSubmit}>
                 <Grid container spacing={2}>
-                    <Grid item xs={12} sm={4}>
-                        <Container>
-                            <Table sx={{ border: "1px solid black" }}>
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell align="center" colSpan={2}>
-                                            Length
-                                        </TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    <TableRow>
-                                        <TableCell align="right">
-                                            <TextField id="lm" name="lm" value={formik.values.lm} onChange={formik.handleChange} />
-                                        </TableCell>
-                                        <TableCell align="left">
-                                            <Stack spacing={0}>
-                                                <TextField id="lt" name="lt" sx={{ borderBottom: "1px solid black" }} value={formik.values.lt} onChange={formik.handleChange} />
-                                                <TextField id="lb" name="lb" sx={{ borderTop: "1px solid black" }} value={formik.values.lb} onChange={formik.handleChange} />
-                                            </Stack>
-                                        </TableCell>
-                                    </TableRow>
-                                </TableBody>
-                            </Table>
-                        </Container>
-                    </Grid>
-                    <Grid item xs={12} sm={4}>
-                        <Container>
-                            <Table sx={{ border: "1px solid black" }}>
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell align="center" colSpan={2}>
-                                            Width
-                                        </TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    <TableRow>
-                                        <TableCell align="right">
-                                            <TextField id="wm" name="wm" value={formik.values.wm} onChange={formik.handleChange} />
-                                        </TableCell>
-                                        <TableCell align="left">
-                                            <Stack spacing={0}>
-                                                <TextField id="wt" name="wt" sx={{ borderBottom: "1px solid black" }} value={formik.values.wt} onChange={formik.handleChange} />
-                                                <TextField id="wb" name="wb" sx={{ borderTop: "1px solid black" }} value={formik.values.wb} onChange={formik.handleChange} />
-                                            </Stack>
-                                        </TableCell>
-                                    </TableRow>
-                                </TableBody>
-                            </Table>
-                        </Container>
-                    </Grid>
-                    <Grid item xs={12} sm={4}>
-                        <Container>
-                            <Table sx={{ border: "1px solid black" }}>
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell align="center" colSpan={2}>
-                                            Height
-                                        </TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    <TableRow>
-                                        <TableCell align="right">
-                                            <TextField id="hm" name="hm" value={formik.values.hm} onChange={formik.handleChange} />
-                                        </TableCell>
-                                        <TableCell align="left">
-                                            <Stack spacing={0}>
-                                                <TextField id="ht" name="ht" sx={{ borderBottom: "1px solid black" }} value={formik.values.ht} onChange={formik.handleChange} />
-                                                <TextField id="hb" name="hb" sx={{ borderTop: "1px solid black" }} value={formik.values.hb} onChange={formik.handleChange} />
-                                            </Stack>
-                                        </TableCell>
-                                    </TableRow>
-                                </TableBody>
-                            </Table>
-                        </Container>
+                    <Grid item xs={12}>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell align="center">Length</TableCell>
+                                    <TableCell align="center">Width</TableCell>
+                                    <TableCell align="center">Height</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                <TableRow>
+                                    <TableCell align="center"><TextField>length</TextField></TableCell>
+                                    <TableCell align="center"><TextField>Width</TextField></TableCell>
+                                    <TableCell align="center"><TextField>Height</TextField></TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
                     </Grid>
                 </Grid>
-                <Box>
-                    <Checkbox id="weight" name="weight" label="Add additional 2x4s for support?" value={formik.values.weight} onChange={formik.handleChange} />
-                    Add additional 2x4s for support?
-                </Box>
-                <Box>
-                    <Button type="submit" variant="outlined">Submit</Button>
-                </Box>
             </form >
         </div >
     )
